0e16094be13da510e6cb60a1630966c8f1367b94
Pavel <lmr_16@mail.ru>
2022-11-18 14:39:02 +0300

    create app and setup settings for start project

---
+++ ToDoList/TaskListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TaskListViewController.swift
+//  ToDoList
+//
+//  Created by user on 18.11.2022.
+//
+
+import UIKit
+
+class TaskListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
+        title = "Task List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.backgroundColor = .blue
+        
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+    }
+
+    
+
+}
+
